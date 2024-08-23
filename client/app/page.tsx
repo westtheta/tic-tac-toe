97fe80dbf6166fea54e60fe257daf212dc8c9615
Okereke Clement Kalu <inspikalu@gmail.com>
2024-08-23 19:37:00 +0100

    Wagwan 22

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -91,6 +91,8 @@ export default function Home() {
       socket.off("gameOver");
       socket.off("gameReset");
       socket.off("opponentQuit");
+      socket.off("playerQuit");
+      socket.off("playerDisconnected");
     };
   }, []);
 
