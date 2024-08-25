ab203f73198e9bbd05aa5086778ef474bac97cfd
Okereke Clement Kalu <inspikalu@gmail.com>
2024-08-25 23:58:54 +0100

    ..

--- proxy/server.js
+++ proxy/server.js
@@ -8,6 +8,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(
   "/socket.io",
   createProxyMiddleware({
