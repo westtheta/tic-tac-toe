f25f4726f1d0ed3783d45e70f918fc8f20cf5832
Okereke Clement Kalu <inspikalu@gmail.com>
2024-08-26 00:31:57 +0100

    proxy doings

--- proxy/server.js
+++ proxy/server.js
@@ -1,53 +1,23 @@
-const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const http = require("http");
+const httpProxy = require("http-proxy");
 
-const app = express();
+const proxy = httpProxy.createProxyServer({ ws: true });
 
-app.use((req, res, next) => {
-  console.log(`Received request for ${req.url}`);
-  next();
+const server = http.createServer((req, res) => {
+  if (req.url === "/cron" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Wagwan my bro");
+  } else {
+    proxy.web(req, res, { target: "https://tic-tac-toe-28r3.onrender.com" });
+  }
 });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
-// app.use(
-//   "/socket.io",
-//   createProxyMiddleware({
-//     target: "https://tic-tac-toe-28r3.onrender.com",
-//     changeOrigin: true,
-//     ws: true,
-//     logLevel: "debug",
-//     pathRewrite: {
-//       "^/socket.io": "/socket.io", // This should match your server's expectation
-//     },
-//   })
-// );
-
-app.use(
-  "/socket.io",
-  createProxyMiddleware({
+server.on("upgrade", (req, socket, head) => {
+  proxy.ws(req, socket, head, {
     target: "https://tic-tac-toe-28r3.onrender.com",
-    changeOrigin: true,
-    ws: true,
-    logLevel: "debug",
-  })
-);
-
-
-app.get("/", (req, res) => {
-  res.send("WebSocket Proxy Server");
+  });
 });
 
-app.get("/cron", (req, res) => {
-  res.send("Wagwan my bro");
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Proxy server is running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log("Proxy server with cron route is running on port 3000");
 });
