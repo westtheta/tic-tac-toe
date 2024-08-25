6f940fd6a675d263ad53a21dedafaee0e63b5d61
Okereke Clement Kalu <inspikalu@gmail.com>
2024-08-25 23:32:04 +0100

    Commit cannot be blank

--- proxy/server.js
+++ proxy/server.js
@@ -1,29 +1,27 @@
-const express = require(express);
-const { createProxyMiddleware } = require(http-proxy-middleware);
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
 // Proxy middleware
 app.use(
-  /socket.io,
+  "/socket.io",
   createProxyMiddleware({
-    target: https://tic-tac-toe-28r3.onrender.com, // Your original server
+    target: "https://tic-tac-toe-28r3.onrender.com", // Your original server
     changeOrigin: true,
     ws: true, // Enable WebSocket proxying
   })
 );
 
+app.get("/", (req, res) => {
+  res.send("WebSocket Proxy Server");
+});
+
 app.get("/cron", (req, res) => {
   res.send("Wagwan my bro");
 });
 
-
-app.get(/, (req, res) => {
-  res.send(WebSocket Proxy Server);
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log();
+  console.log(`Proxy server is running on port ${PORT}`);
 });
-
