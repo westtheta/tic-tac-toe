0f92788da9e4b6f8458da93be901d4d4a8fbdd14
Okereke Clement Kalu <inspikalu@gmail.com>
2024-08-26 13:42:58 +0100

    Dogs listed today

--- proxy/server.js
+++ proxy/server.js
@@ -1,24 +1,30 @@
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-const http = require('http');
-const httpProxy = require('http-proxy');
+const http = require("http");
+const httpProxy = require("http-proxy");
 
 const proxy = httpProxy.createProxyServer({ ws: true });
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/cron' && req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Wagwan my bro');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");zzzzz
+
+  if (req.url === "/cron" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Wagwan my bro");
   } else {
-    proxy.web(req, res, { target: 'https://tic-tac-toe-28r3.onrender.com', changeOrigin: true });
+    proxy.web(req, res, {
+      target: "https://tic-tac-toe-28r3.onrender.com",
+      changeOrigin: true,
+    });
   }
 });
 
-server.on('upgrade', (req, socket, head) => {
-  proxy.ws(req, socket, head, { target: 'https://tic-tac-toe-28r3.onrender.com' });
+server.on("upgrade", (req, socket, head) => {
+  proxy.ws(req, socket, head, {
+    target: "https://tic-tac-toe-28r3.onrender.com",
+  });
 });
 
 server.listen(3000, () => {
-  console.log('Proxy server is running on port 3000');
+  console.log("Proxy server is running on port 3000");
 });
