4b73035b83939af48e982fe8fb4bec3060247c63
Okereke Clement Kalu <inspikalu@gmail.com>
2024-08-27 22:58:40 +0100

    Updated vercel.json

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,22 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   async headers() {
+  async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Content-Security-Policy',
+            key: "Content-Security-Policy",
             value: `
               default-src 'self';
               script-src 'self' 'unsafe-inline' 'unsafe-eval';
               style-src 'self' 'unsafe-inline';
               img-src 'self' data:;
-              connect-src 'self' https://tic-tac-toe-28r3.onrender.com;
+              connect-src 'self' https://tic-tac-toe-28r3.onrender.com https://tic-tac-toe-29r3.onrender.com;
               font-src 'self' data:;
               object-src 'none';
               frame-ancestors 'none';
-            `.replace(/\s{2,}/g, ' ').trim(),
+            `
+              .replace(/\s{2,}/g, " ")
+              .trim(),
           },
         ],
       },
